f05ff8353998181b7b3c486a21c45beb8bb1646f
mbilal2024 <abdullahcloudupload@gmail.com>
2024-06-11 11:03:41 +0500

    Structure

---
+++ cityguide/lib/Theme/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Theme{
+ static Color primaryColor = Colors.blue;
+
+ static Color secondaryColor = Color.fromARGB(255, 136, 197, 247);
+ static Color lightColor = Color.fromARGB(255, 185, 216, 241) ;
+}
