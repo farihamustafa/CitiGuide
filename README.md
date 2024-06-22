11bf119a9fc0e695b2b900431133ab9d581d4539
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-06-22 10:55:33 +0500

    updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CitiGuide
+//gradlew signingReport
--- cityguide/lib/components/reusable/appbar.dart
+++ cityguide/lib/components/reusable/appbar.dart
@@ -1,4 +1,6 @@
+import 'package:cityguide/Pages/cityscreen.dart';
 import 'package:cityguide/Pages/login.dart';
+import 'package:cityguide/Theme/color.dart';
 import 'package:cityguide/components/reusable/materialbutton.dart';
 import 'package:cityguide/components/reusable/sizedbox.dart';
 import 'package:flutter/material.dart';
@@ -6,49 +8,51 @@ import 'package:get/get.dart';
 
 app_Bar() {
   return AppBar(
+    automaticallyImplyLeading: false,
     title: const Text('City Guide'),
     surfaceTintColor: Colors.transparent,
     backgroundColor: Colors.transparent,
     actions: [
       IconButton(
           onPressed: () {
-            Get.defaultDialog(
-                barrierDismissible: false,
-                title: "Alert!",
-                content: Text("Do you want to Logout?"),
-                actions: [
-                  materialButton(
-                      function: () {
-                        Get.snackbar("Logout", "Logout successfully");
-                        Get.to(Login());
-                      },
-                      btnText: "yes",
-                      btnColor: Colors.green),
-                  materialButton(
-                      function: () {
-                        Get.back();
-                      },
-                      btnText: "No",
-                      btnColor: Colors.red)
-                ]);
             // Get.defaultDialog(
-            //     onConfirm: () => {
-            //           Get.snackbar("Logout", "Logout successfully"),
-            //           Get.to(Login())
-            //         },
-            //     onCancel: () => {Get.back()},
-            //     title: "Logout",
-            //     content: Column(
-            //       mainAxisAlignment: MainAxisAlignment.center,
-            //       children: [
-            //         Icon(
-            //           Icons.question_mark,
-            //           size: 40,
-            //         ),
-            //         sizedBox(),
-            //         Text("Are you sure to logout!"),
-            //       ],
-            //     ));
+               
+            //     title: "Alert!",
+            //     content: Text("Do you want to Logout?"),
+            //     actions: [
+            //       materialButton(
+            //           function: () {
+            //             Get.snackbar("Logout", "Logout successfully");
+            //             Get.offAll(Login());
+                      
+            //           },
+            //           btnText: "yes",
+            //           btnColor: ColorTheme.primaryColor),
+            //       materialButton(
+            //           function: () {
+            //             Get.back();
+            //           },
+            //           btnText: "No",
+            //           btnColor: Colors.red)
+            //     ]);
+            Get.defaultDialog(
+                onConfirm: () => {
+                      Get.snackbar("Logout", "Logout successfully"),
+                      Get.to(Login())
+                    },
+                onCancel: () => {Get.back()},
+                title: "Logout",
+                content: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.question_mark,
+                      size: 40,
+                    ),
+                    sizedBox(),
+                    Text("Are you sure to logout!"),
+                  ],
+                ));
           },
           icon: const Icon(Icons.logout))
     ],
